a706f9c2fc04d4a73426ac645a029d02cbe56e53
Xenorio <dev@xenorio.xyz>
2023-01-02 09:11:46 +0100

    particles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { Engine } from "tsparticles-engine";
+	import Particles from "svelte-particles";
+    import { loadFull } from "tsparticles";
+	import {particlesConfig} from "$lib/particles.json"
+
+    let onParticlesLoaded = (event: any) => {
+        const particlesContainer = event.detail.particles;
+
+        // you can use particlesContainer to call all the Container class
+        // (from the core library) methods like play, pause, refresh, start, stop
+    };
+
+    let particlesInit = async (engine: Engine) => {
+        // you can use main to customize the tsParticles instance adding presets or custom shapes
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(engine);
+    };
+</script>
+
+<Particles
+    id="tsparticles"
+    options="{particlesConfig}"
+    on:particlesLoaded="{onParticlesLoaded}"
+    particlesInit="{particlesInit}"
+/>
+
+<style lang="scss">
+</style>
