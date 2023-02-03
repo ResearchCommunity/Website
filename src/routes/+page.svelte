7dc4e3d325ca249c5df6e9949b76e5e6daafd488
Xenorio <dev@xenorio.xyz>
2023-02-03 14:56:04 +0100

    content™

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import type { Engine } from 'tsparticles-engine';
 	import Particles from 'svelte-particles';
 	import { loadFull } from 'tsparticles';
-	import { particlesConfig } from '$lib/particles.json';
 	import { Button } from 'sveltestrap';
 
 	let particlesInit = async (engine: Engine) => {
@@ -10,36 +9,138 @@
 	};
 </script>
 
-<Particles id="tsparticles" options={particlesConfig} {particlesInit} />
-
-<div id="content">
-	<h1>Hello There!</h1>
-	<h2>We're still working on this site...</h2>
-	<h3>
-		In the meantime, you can join our
-		<a href="https://discord.com/invite/ZSmu4hmabs" target="_blank" rel="noreferrer">
-			<Button color="primary"> <i class="fa-brands fa-discord"/> Discord Server</Button>
-		</a>
-	</h3>
+<div id="header" class="container w-100 noselect">
+	<img src="/logo.png" alt="Logo" id="logo" />
+	<h1>Research Community</h1>
 </div>
 
+<div id="content">
+	<div class="content-box">
+		<h1>About Us</h1>
+		<p>
+			Welcome to the Research Community! <br />
+			We're an open and friendly community of people interested in and working with research chemicals!
+			<br /><br />
+
+			Most of what we do centers around education and harm reduction on research chemicals and other
+			psychoactive substances. <br /> <br />
+
+			Come join and chat with us!
+		</p>
+	</div>
+	<div class="content-box">
+		<h1>Visit Us!</h1>
+		<a href="https://discord.com/invite/ZSmu4hmabs" target="_blank" rel="noopener noreferrer"
+			><Button color="primary"><i class="fa-brands fa-discord" /> Join on Discord</Button></a
+		>
+	</div>
+	<div class="content-box">
+		<h1>Social Media</h1>
+		<a href="https://instagram.com/research_community_" target="_blank" rel="noopener noreferrer"
+			><Button color="warning"><i class="fa-brands fa-instagram" /> Instagram</Button></a
+		>
+	</div>
+</div>
+
+<div id="footer">
+	<div class="row">
+		<div class="col-md-4">
+			<a href="/disclosure">Legal Disclosure &amp; Privacy Policy</a>
+		</div>
+		<div class="col-md-4">
+			Made with ❤️ and <img src="/svelte-logo.png" alt="Svelte" style="width: 1rem;" /> in 🇩🇪
+		</div>
+		<div class="col-md-4">
+			© 2023 Marcus Huber (xenorio) | Licensed under <a
+				href="https://github.com/ResearchCommunity/Website/blob/main/LICENSE"
+				target="_blank"
+				rel="noopener noreferrer">AGPLv3</a
+			>
+		</div>
+	</div>
+</div>
+
+<Particles id="tsparticles" url="/particles.json" {particlesInit} />
+
 <style>
-	#content {
-		position: absolute;
-		left: 50%;
-		top: 50%;
-		transform: translateX(-50%) translateY(-50%);
+	@import url('https://fonts.googleapis.com/css2?family=Concert+One&family=Poppins&display=swap');
+
+	#header {
 		text-align: center;
+		font-family: 'Concert One', cursive;
+		color: white;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translateX(-50%) translateY(-50%);
+	}
+
+	#header h1 {
+		font-size: 5rem;
+		color: #bb1fc0;
+		text-shadow: 2px 2px 4px #000000;
+	}
+
+	#content {
+		text-align: center;
+		font-family: 'Poppins', sans-serif;
+		margin-top: 100vh;
+	}
+	.content-box {
 		background-color: #242424;
 		border-radius: 25px;
-		color: white;
 		padding: 3rem;
-		min-width: 25%;
+		max-width: 75%;
+		display: block;
+		color: white;
+		margin-bottom: 5rem;
+		font-size: x-large;
+		margin-left: 50%;
+		transform: translateX(-50%);
+	}
+
+	.content-box h1 {
+		font-family: 'Concert One', cursive;
+		margin-bottom: 2rem;
+	}
+
+	#logo {
+		min-width: 200px;
+		max-width: 25%;
+		border-radius: 50%;
+	}
+
+	.noselect {
+		-webkit-user-select: none;
+		-moz-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+	}
+
+	#footer {
+		width: 100%;
+		min-height: 3rem;
+		background-color: #242424;
+		color: white;
+		padding: 1rem;
+		text-align: center;
+	}
+
+	#footer a {
+		text-decoration: none;
+	}
+
+	#footer .row {
+		width: 100%;
 	}
 
 	@media screen and (max-width: 800px) {
-		#content {
+		.content-box {
 			width: 100%;
+		}
+
+		#footer .col-md-4 {
+			margin-top: 1rem;
 		}
 	}
 </style>
---
+++ src/routes/disclosure/+page.svelte
@@ -0,0 +1,153 @@
+<script lang="ts">
+	import type { Engine } from 'tsparticles-engine';
+	import Particles from 'svelte-particles';
+	import { loadFull } from 'tsparticles';
+	import { Button } from 'sveltestrap';
+
+	let particlesInit = async (engine: Engine) => {
+		await loadFull(engine);
+	};
+</script>
+
+<a href="/" id="homebutton"><Button color="warning"><i class="fa-solid fa-house"></i></Button></a>
+
+<div id="content">
+	<div class="content-box">
+		<h1>Legal Disclosure</h1>
+
+		Information in accordance with Section 5 TMG <br>
+        (German law stuff)<br /><br />
+
+		<h3>Represented By</h3>
+		Xenorio - Management <br>
+        Marcus Huber <br>
+        Postfach 22 45 <br>
+        64533 Mörfelden-Walldorf <br>
+        Germany <br><br>
+
+		<h3>Contact Information</h3>
+		E-Mail: business@xenorio.xyz <br>
+        Phone: +49 160 897 8834
+        <br><br>
+
+        <h3>Accountability for Content</h3>
+        "Xenorio - Management" is fully responsible for any content found on this website. <br><br>
+        Please note that we are not obliged to monitor transmitted or saved information of third parties. <br><br>
+        Also, we are not obliged to investigate circumstances pointing to illegal activity.
+        <br><br>
+
+        <h3>Accountability for Links</h3>
+        Responsibility for content on websites of third parties which are linked on this website lies with the operators of the linked websites. <br><br>
+        At the time of linking, no violations were evident to us.
+
+	</div>
+	<div class="content-box">
+		<h1>Privacy Policy</h1>
+		
+        <h3>Cookies &amp; Trackers</h3>
+        We do not set or use any cookies or other means of tracking <br><br>
+
+        <h3>Data Processing</h3>
+        This website actively processes and saves your IP address. This is used to provide basic functionality, and prevent potential malicious acts.
+        <br><br>
+        Your browser or operating system may send other personally identifying data. This data is being ignored.
+        <br><br>
+        Our servers are entirely located in Europe (Finland). We abide by european data protection laws. 
+        <br><br>
+
+        <h3>3rd Parties</h3>
+        Some resources are being loaded from 3rd parties. <br>
+        These are: <br><br>
+        <ul>
+            <li>cloudflare.com | FontAwesome Library</li>
+            <li>jsdelivr.net | Bootstrap Library</li>
+            <li>google.com | Fonts</li>
+        </ul>
+        <br>
+        By loading these resources, you are potentially sending personally identifying information to these services. 
+	</div>
+</div>
+
+<div id="footer">
+	<div class="row">
+		<div class="col-md-4">
+			<a href="/disclosure">Legal Disclosure &amp; Privacy Policy</a>
+		</div>
+		<div class="col-md-4">
+			Made with ❤️ and <img src="/svelte-logo.png" alt="Svelte" style="width: 1rem;" /> in 🇩🇪
+		</div>
+		<div class="col-md-4">
+			© 2023 Marcus Huber (xenorio) | Licensed under <a
+				href="https://github.com/ResearchCommunity/Website/blob/main/LICENSE"
+				target="_blank"
+				rel="noopener noreferrer">AGPLv3</a
+			>
+		</div>
+	</div>
+</div>
+
+<Particles id="tsparticles" url="/particles.json" {particlesInit} />
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Concert+One&family=Poppins&display=swap');
+
+	#content {
+		text-align: center;
+		font-family: 'Poppins', sans-serif;
+		margin-top: 3rem;
+	}
+	.content-box {
+		background-color: #242424;
+		border-radius: 25px;
+		padding: 3rem;
+		max-width: 75%;
+		display: block;
+		color: white;
+		margin-bottom: 5rem;
+		font-size: x-large;
+		margin-left: 50%;
+		transform: translateX(-50%);
+	}
+
+	.content-box h1 {
+		font-family: 'Concert One', cursive;
+		margin-bottom: 2rem;
+	}
+
+    .content-box h3 {
+		text-decoration: underline;
+	}
+
+	#footer {
+		width: 100%;
+		min-height: 3rem;
+		background-color: #242424;
+		color: white;
+		padding: 1rem;
+		text-align: center;
+	}
+
+	#footer a {
+		text-decoration: none;
+	}
+
+	#footer .row {
+		width: 100%;
+	}
+
+    #homebutton {
+        position: absolute;
+        top: 2rem;
+        left: 2rem;
+    }
+
+	@media screen and (max-width: 800px) {
+		.content-box {
+			width: 100%;
+		}
+
+		#footer .col-md-4 {
+			margin-top: 1rem;
+		}
+	}
+</style>
