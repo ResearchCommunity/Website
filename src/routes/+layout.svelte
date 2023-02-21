1e065c2b8e8b81c90bdfddf2afb3cadcd314d4c1
Xenorio <dev@xenorio.xyz>
2023-02-21 06:23:38 +0100

    edit head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,16 @@
 	};
 </script>
 
+<svelte:head>
+	<title>Research Community</title>
+
+	<meta content="Research Community" property="og:title" />
+	<meta content="Community around Research and Education on Psychoactive Substances" property="og:description" />
+	<meta content="https://research-comunity.org" property="og:url" />
+	<meta content="https://research-community.org/logo.png" property="og:image" />
+	<meta content="#6441A4" data-react-helmet="true" name="theme-color" />
+</svelte:head>
+
 <slot />
 
 <Particles id="tsparticles" url="/particles.json" {particlesInit} />
