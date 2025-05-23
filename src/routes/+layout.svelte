6d8dd9ef7515f5b61ffd84df73b5a617790e08ac
xenorio <dev@xenorio.xyz>
2025-05-23 23:22:18 +0200

    mobile optimization

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,8 @@
 	import { LightSwitch } from "@skeletonlabs/skeleton";
 	import { AppBar } from "@skeletonlabs/skeleton";
 	import "../app.scss";
-	import 'animate.css';
-	import '@fortawesome/fontawesome-free/css/all.min.css'
+	import "animate.css";
+	import "@fortawesome/fontawesome-free/css/all.min.css";
 	let { children } = $props();
 </script>
 
@@ -24,21 +24,44 @@
 </div>
 
 <div id="footer">
-	<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-		<svelte:fragment slot="lead">&copy; 2025 Xenorio Industries - AGPLv3 Licensed</svelte:fragment>
-		Made with 💜 and <img src="/svelte-logo.png" alt="Svelte" style="width: 1rem; display: inline-block;">
+	<AppBar
+		gridColumns="grid-cols-3"
+		slotDefault="place-self-center"
+		slotTrail="place-content-end"
+	>
+		<svelte:fragment slot="lead"
+			><div id="footer-lead">
+				&copy; 2025 Xenorio Industries - AGPLv3 Licensed
+			</div></svelte:fragment
+		>
+		<div id="footer-content">
+			Made with 💜 and
+			<img
+				src="/svelte-logo.png"
+				alt="Svelte"
+				style="width: 1rem; display: inline-block;"
+			/>
+		</div>
 		<svelte:fragment slot="trail">
-			<a href="/disclosure">Legal Disclosure & Privacy Policy</a>
-			&nbsp; | &nbsp;
-			<a href="https://github.com/ResearchCommunity/Website" target="_blank">Source Code</a>
+			<div id="footer-links">
+				<div><a href="/disclosure">Legal Disclosure & Privacy Policy</a></div>
+				<span class="footer-divider">&nbsp;|&nbsp;</span>
+				<hr class="footer-divider-mobile" />
+				<div>
+					<a
+						href="https://github.com/ResearchCommunity/Website"
+						target="_blank"
+					>
+						Source Code
+					</a>
+				</div>
+			</div>
 		</svelte:fragment>
 	</AppBar>
 </div>
 
 <svelte:head>
-	<title>
-		Research Community
-	</title>
+	<title>Research Community</title>
 </svelte:head>
 
 <style>
@@ -50,7 +73,7 @@
 	}
 
 	#content {
-		flex: 1
+		flex: 1;
 	}
 
 	#footer {
@@ -59,5 +82,50 @@
 
 	#footer a {
 		margin-left: 0;
+	}
+
+	#footer-links {
+		display: flex;
+		align-items: center;
+	}
+
+	.footer-divider {
+		display: inline;
+	}
+
+	.footer-divider-mobile {
+		display: none;
+	}
+
+	@media (max-width: 900px) {
+		#footer-links {
+			flex-direction: column;
+			align-items: center;
+			text-align: center;
+			gap: 0.5rem;
+		}
+
+		.footer-divider {
+			display: none;
+		}
+
+		.footer-divider-mobile {
+			display: block;
+			width: 100%;
+		}
+
+		#footer #footer-content {
+			text-align: center;
+			margin-top: 0.5rem;
+		}
+
+		#footer-lead {
+			text-align: center;
+			width: 100%;
+		}
+
+		#footer a {
+			margin-left: 0;
+		}
 	}
 </style>
